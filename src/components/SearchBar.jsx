c94162690603584a9963147d38dbb187dc1a46eb
ISABEL <cg3026337@labA204m07.IFSP>
2024-05-13 09:31:16 -0300

    Galeria de Imagens

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const SearchBar = () => {
+    <div>SearchBar</div>
+    const categorias = [
+        'Natureza',
+        'Pessoas',
+        'Tecnologia',
+        'Animais',
+        'Esportes',
+    ];
+    
+    return (
+        <div className='search-bar'>
+            <input type="text" placeholder='Pesquisar fotos...' />
+            <button>Pesquisar</button>
+            <select>
+                {categorias.map((categoria) => (
+                    <option key={categoria} value={categoria}>
+                        {categoria}
+                    </option>
+                ))}
+            </select>
+        </div>
+    ) 
+};
+
+export default SearchBar;
